1c7816f888019b9a8092c37708d5be599ee42d22
Mehmet Kaan KARAGÖZOĞLU <55835876+Dtractus@users.noreply.github.com>
2024-10-21 12:01:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -36,11 +36,13 @@ account = web3.eth.account.from_key(private_key)
 contract_address = web3.to_checksum_address(contract_address)
 
 # Method ID for claim() function
-method_id = web3.keccak(text="claim()")[:4].hex()
+method_id = "0x4e71d92d"
+gas_limit = 300000
+gas_price = web3.eth.getGasPrice()
 
 def claimable_frag():
     try:
-        data = f"0x89885049000000000000000000000000{account.address[2:]}"
+        data = f"0x02d9c737000000000000000000000000{account.address[2:]}"
         from_address = web3.to_checksum_address('0x24edfad36015420a84573684644f6dc74f0ba8c5')
         response = web3.eth.call({
             'from': from_address,
@@ -62,8 +64,8 @@ def claim():
             'from': account.address,
             'to': contract_address,
             'nonce': nonce,
-            'gas': 300000,
-            'gasPrice': web3.to_wei('0.01', 'gwei'),
+            'gas': gas_limit,
+            'gasPrice': web3.utils.toHex(gas_price),
             'data': method_id
         }
         signed_tx = web3.eth.account.sign_transaction(tx, private_key)
